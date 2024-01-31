148d1b211db12ca4c0733b48fac938b223eeec4d
AntonMiranovich <antonmir992@gmail.com>
2024-01-31 20:46:34 +0300

    ep4

--- react-tasks/src/pages/Task19/Task19.js
+++ react-tasks/src/pages/Task19/Task19.js
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom";
 
 function Task19() {
+  const arr = [1, 2, 3, 4];
+
+  function resSum() {
+    console.log(
+      arr.reduce((sum, el) => {
+        return sum + el;
+      }, 0)
+    );
+  }
+
   return (
     <>
       <h1>Задача №19</h1>
@@ -8,6 +18,9 @@ function Task19() {
         Реализуйте компонент с массивом чисел и кнопкой. При клике на кнопку
         вычислите и выведите сумму всех чисел из массива в консоль.
       </p>
+
+      <p>массив чисел {arr}</p>
+      <button onClick={resSum}>Сlick to find out the sum of the array</button>
 
       <Link to={"/"}> Перейти на главную страницу</Link>
     </>
--- react-tasks/src/pages/Task20/Task20.js
+++ react-tasks/src/pages/Task20/Task20.js
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 
 function Task20() {
+  const arr = [1, 4, "anton", "weqeadsd", 7];
+
+  function randomEl() {
+    console.log(arr[Math.floor(Math.random() * 5)]);
+  }
+
   return (
     <>
       <h1>Задача №20</h1>
@@ -9,6 +15,9 @@ function Task20() {
         кнопку выбирайте случайный элемент из массива и отображайте его в
         консоль.
       </p>
+
+      <p>массив элементов {arr}</p>
+      <button onClick={randomEl}>Click me</button>
 
       <Link to={"/"}> Перейти на главную страницу</Link>
     </>
