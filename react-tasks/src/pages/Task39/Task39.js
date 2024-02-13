31c7d342a5913e89ceb0674385c78608d704e109
AntonMiranovich <antonmir992@gmail.com>
2024-02-13 22:27:39 +0300

    task 39

--- react-tasks/src/pages/Task39/Task39.js
+++ react-tasks/src/pages/Task39/Task39.js
@@ -2,15 +2,18 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 function Task39() {
+  const [inpValues] = useState([]);
 
   return (
     <>
       <h1>Задача №39</h1>
       <p>
-
+        получить значение из 2 инпутов и отобразить это в консоль по клику на
+        кнопку (одним useState)
       </p>
-
-
+      <input onChange={(e) => (inpValues[0] = e.target.value)} />
+      <input onChange={(e) => (inpValues[1] = e.target.value)} />
+      <button onClick={() => console.log(inpValues[0], inpValues[1])}>Click me</button>
 
       <Link to={"/"}> Перейти на главную страницу</Link>
     </>
